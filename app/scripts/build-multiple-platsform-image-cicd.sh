b8d13a6e5526e85bb05c1315348b487a8917af08
xinxin.he <cs_hexinxin@163.com>
2023-12-16 12:35:52 +0800

    remove emoji because github action not suppport

--- app/scripts/build-multiple-platsform-image-cicd.sh
+++ app/scripts/build-multiple-platsform-image-cicd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Start to build and push arm64 images to docker hub"
 docker buildx build  -t csmervyn718/gradle-java-project-template:latest --platform=linux/arm64 . --push
-echo "ARM64 Images push to docker hub success U+2705"
+echo "ARM64 Images push to docker hub success"
 echo "Start to build and push amd64 images to docker hub"
 docker buildx build  -t csmervyn718/gradle-java-project-template:latest --platform=linux/amd64 . --push
-echo "AMD64 Images push to docker hub success U+2705"
+echo "AMD64 Images push to docker hub success"
